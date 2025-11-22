20fc51894085ee26cf508693ddb6bfce34b4114c
Aashish Chaudhary <mraasary@gmail.com>
2025-11-22 18:47:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -907,7 +907,7 @@ app.get('/api/tasks/:email', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Error handling for server
 const server = app.listen(PORT, '0.0.0.0', (error) => {
